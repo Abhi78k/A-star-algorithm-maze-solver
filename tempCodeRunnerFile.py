0a3f945a2a97f9e8d14a4c9403fd945d100b56df
Abhi78k <abhijithk7800@gmail.com>
2025-02-24 21:51:04 +0530

    final code for the maze solver

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+temp_g_score = g_score[currCell]+1
+      # # temp_f_score = temp_g_score + h(childCell,(1,1))
+      
+      # # if temp_f_score<f_score[childCell]:
+      # #   g_score[childCell]=temp_g_score
+      # #   f_score[childCell]=temp_f_score
+      # #   open.put((temp_f_score,h(childCell,(1,1)),childCell))
